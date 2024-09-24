e3f456d8f18226f5e2770b0b7c4a72d06cc9a71c
Diego-BP25 <alejandroberrio0525@gmail.com>
2024-09-24 09:50:12 -0500

    modificacion del comando wallet 4.0

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,8 +1,8 @@
 const user = require("../model/user.model");
 const { USER } = require("../config/config.constant");
 
-
 class userServices {
+    // Guardar un usuario en la base de datos
     async saveUser(data) {
         try {
             const { id, wallet_address, encryptedPrivateKey } = data;
@@ -27,38 +27,62 @@ class userServices {
             }
         } catch (error) {
             return {
-                message: USER.ERROR + error.message, 
+                message: USER.ERROR + error.message,
                 success: false,
             };
         }
     }
 
-
+    // Recuperar las wallets de un usuario
     async fetchWallet(id) {
         try {
-          const getUser = await user.findOne({ userId: id });
-          if (getUser) {
-            return {
-              message: USER.WALLET_FETCHED,
-              success: true,
-              wallets: getUser.wallets, // Devolvemos todas las wallets del usuario
-            };
-          } else {
-            return {
-              message: USER.WALLET_NOT_FETCHED,
-              success: false,
-            };
-          }
+            const getUser = await user.findOne({ userId: id });
+            if (getUser) {
+                return {
+                    message: USER.WALLET_FETCHED,
+                    success: true,
+                    wallets: getUser.wallets, // Devolvemos todas las wallets del usuario
+                };
+            } else {
+                return {
+                    message: USER.WALLET_NOT_FETCHED,
+                    success: false,
+                };
+            }
         } catch (error) {
-          return {
-            message: USER.ERROR + error,
-            success: false,
-          };
+            return {
+                message: USER.ERROR + error,
+                success: false,
+            };
         }
-      }
-      
+    }
 
+    // Recuperar todas las wallets de un usuario (nueva función)
+    async fetchAllWallets(userId) {
+        try {
+            const getUser = await user.findOne({ userId });
+            if (getUser && getUser.wallet_address) {
+                return {
+                    message: USER.WALLETS_FETCHED,
+                    success: true,
+                    wallets: [{ wallet_address: getUser.wallet_address }] // Devolver las wallets encontradas
+                };
+            } else {
+                return {
+                    message: USER.WALLETS_NOT_FOUND,
+                    success: false,
+                    wallets: []
+                };
+            }
+        } catch (error) {
+            return {
+                message: USER.ERROR + error.message,
+                success: false,
+            };
+        }
+    }
 
+    // Recuperar la clave privada cifrada de un usuario
     async fetch_Private_key(id) {
         try {
             const fetch_user = await user.findOne({ userId: id });
@@ -82,9 +106,10 @@ class userServices {
         }
     }
 
+    // Actualizar información de un usuario
     async UpdateUser(id, data) {
         try {
-            const fetch_user = await user.findOneAndUpdate({ userId: id }, data );
+            const fetch_user = await user.findOneAndUpdate({ userId: id }, data);
             if (fetch_user) {
                 return {
                     message: USER.USER_UPDATED,
@@ -102,8 +127,7 @@ class userServices {
                 success: false,
             };
         }
-
     }
-
 }
-module.exports = new userServices()
+
+module.exports = new userServices();
