b57d372eac39296c1e8b8b6c1c034226bb286a70
Diego-BP25 <alejandroberrio0525@gmail.com>
2024-11-06 16:45:27 -0500

    swap token a trx 1.2

--- src/commands/swapToken.js
+++ src/commands/swapToken.js
@@ -66,7 +66,9 @@ async function listWallets(ctx) {
   // Manejador para la selección de wallet y para solicitar la dirección del token
 async function handleAskToken(ctx) {
     const callbackData = ctx.update.callback_query.data;
+    console.log("Callback data:", callbackData); // Verifica el valor recibido en el callback
     const addressWallet = callbackData.replace('swap_wallet_token', '');
+    console.log("Parsed wallet address:", addressWallet);
   
     // Guardar la wallet en sesión y cambiar el estado
     ctx.session.walletFrom = addressWallet;
