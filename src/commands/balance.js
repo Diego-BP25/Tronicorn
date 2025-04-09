1b29bf2c4bb3ae2f81728a662f211ef67409d4fc
Diego-BP25 <alejandroberrio0525@gmail.com>
2025-04-09 15:30:15 -0500

    intento para arreglar comando balance

--- src/commands/balance.js
+++ src/commands/balance.js
@@ -133,7 +133,6 @@ async function getTRC20Balance(address) {
 
     for (const asset of assets)  {
       const roundedBalance = parseFloat(asset.balance).toFixed(6);
-      const roundedValueInUSD = parseFloat(asset.token_value_in_usd).toFixed(6);
       const tokenSymbol = asset.token_abbr || ""; // Símbolo del token
       const TokenName= asset.token_name
       let valueInTRX;
@@ -141,6 +140,8 @@ async function getTRC20Balance(address) {
       if (TokenName.toLowerCase() === "trx") {
         valueInTRX = roundedBalance;
       } else {
+        console.log("Token:", TokenName, "ID:", asset.tokenId);
+
         const tokenPriceInTRX = await getTokenPriceInTRX(asset.tokenId);
         valueInTRX = tokenPriceInTRX
           ? (parseFloat(asset.balance) * tokenPriceInTRX).toFixed(6)
