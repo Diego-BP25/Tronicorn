546e712317072734b168f483b35735dd1bf7f213
Diego-BP25 <alejandroberrio0525@gmail.com>
2025-04-18 21:07:53 -0500

    arreglar vista del menu principal

--- src/commands/start.js
+++ src/commands/start.js
@@ -4,7 +4,8 @@ async function startCommand(ctx) {
   try {
     await ctx.replyWithMarkdown(
 `Welcome to the TRON Bot! Choose an option:`,
-      Markup.inlineKeyboard([
+{
+  reply_markup: Markup.inlineKeyboard([
         [Markup.button.callback(`🔐 *Wallet Management*`, '')],
         [Markup.button.callback('💼 Wallet', 'wallet'), Markup.button.callback('💰 Balance', 'balance'), Markup.button.callback('🌐 Link Wallet', 'external')],
         [Markup.button.callback(`🔄  *Trading Tools*`, '')],
@@ -14,6 +15,7 @@ async function startCommand(ctx) {
         [Markup.button.callback(`💵 *Stable Assets*`, '')],
         [Markup.button.callback('🪙 StableCoins', 'stableCoins')]
       ])
+    }
     );    
   } catch (error) {
     console.error("Error showing menu:", error);
